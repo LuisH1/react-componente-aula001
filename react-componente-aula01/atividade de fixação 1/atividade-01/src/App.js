194880c2bc43276c142443737c170a5df9b335fa
Luis Henrique <LuisHenSilva@Outlook.com>
2021-10-14 12:42:02 -0300

    react  componentes 01

---
+++ react-componente-aula01/atividade de fixação 1/atividade-01/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="titulo">Ter esperança é esperar com fé</h1>
+      <pre className="texto">
+          Sabe qual é a melhor definição de esperança? Esperar com fé! Esperar que os sonhos se realizem, <br/>
+          apesar dos contratempos, esperar com paciência, mas não esperar parado!  <br/>
+          A esperança é o que nos faz, em meio a tempestade, abrir o nosso guarda-chuva colorido!  <br/>
+          A esperança é ver apenas pequenos pontos de luz na escuridão, mas deixar-se iluminar por completo.  <br/>
+          A esperança é ter forças para tentar outra vez, mesmo quando o mundo lhe diz para desistir.  <br/>
+          Ter esperança é ter a coragem de criar expectativas positivas. É pensar que sempre haverá uma saída,  <br/>
+          é saber que não há mal que dure para sempre. Quem tem esperança,   <br/>
+          sabe que a qualquer momento coisas boas podem acontecer. É o impossível se torna possível,  <br/>
+          porque somente no coração em que há esperança acontecem milagres.  <br/>
+          Manter a esperança viva é a maior prova de força que uma pessoa pode dar a si mesmo.  <br/>
+      </pre>
+    </div>
+  );
+}
+
+export default App;
